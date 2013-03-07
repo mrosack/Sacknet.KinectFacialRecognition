b1c409905f906f0724fa5a508afd941c0dc9b8fa
Mike Rosack <mrosack@gmail.com>
2013-03-06 18:07:25 -0600

    add unit test project so we can verify managed version is working correctly when it's ready

---
+++ Sacknet.KinectFacialRecognitionTests/EigenObjectRecognizerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sacknet.KinectFacialRecognition;
+
+namespace Sacknet.KinectFacialRecognitionTests
+{
+    [TestClass]
+    [DeploymentItem("Images")]
+    [DeploymentItem("DLL")]
+    public class EigenObjectRecognizerTest
+    {
+        [TestMethod]
+        public void RecognizerSucessfullyRecognizesMe()
+        {
+            var faces = new List<TargetFace>();
+
+            foreach(var trainingImage in Directory.GetFiles(".", "train*.*"))
+            {
+                faces.Add(new TargetFace
+                {
+                    Key = trainingImage,
+                    Image = new Bitmap(trainingImage)
+                });
+            }
+
+            var termCrit = new Emgu.CV.Structure.MCvTermCriteria(faces.Count, 0.001);
+            var recognizer = new EigenObjectRecognizer(faces, 2000, ref termCrit);
+
+            float eigenDistance;
+            var testImage = new Emgu.CV.Image<Emgu.CV.Structure.Gray, byte>("test_mike.png");
+            var result = recognizer.Recognize(testImage, out eigenDistance);
+
+            Assert.AreEqual(734.0543, Math.Round(eigenDistance, 4));
+            Assert.AreEqual(@".\train_mike_2.png", result);
+        }
+    }
+}
---
+++ Sacknet.KinectFacialRecognitionTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sacknet.KinectFacialRecognitionTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sacknet.KinectFacialRecognitionTests")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9c0680d7-c31a-41b4-b87f-6e1874a7dbbc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
